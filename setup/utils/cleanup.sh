055eb7dc6915b9376cd7878803d00f8697cbf547
Matt Ma <bigmabig@gmail.com>
2015-09-02 16:20:55 -0700

    update the cleanup scripts with more complete solution on remove certs and keys

--- setup/utils/cleanup.sh
+++ setup/utils/cleanup.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 set -e
 
+# Remove the existing VM in the cluster
+
 IP=172.17.8.101 NUM=1 vagrant destroy -f
 
 IP=172.17.8.102 NUM=2 vagrant destroy -f
 
 ROLE=master vagrant destroy -f
 
+
+# Cleanup the keys, hosts info, and kube config
+
 rm -rf ~/.fleetctl/known_hosts
 
 rm -rf ~/.vagrant.d/insecure_private_key
+
+
+# private key of kubeconfig
+rm -rf ~/.kube/config
+
+
+# Cleanup the certs folder
+
+./setup/utils/certs-cleanup.sh
