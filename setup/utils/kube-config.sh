ebc63b515d132b925d342c02e636e4ffd40611fe
Matt Ma <bigmabig@gmail.com>
2015-09-19 19:14:29 -0700

    update the kube-config info

--- setup/utils/kube-config.sh
+++ setup/utils/kube-config.sh
@@ -13,7 +13,7 @@ CONTEXT_NAME=rocks
 kubectl config set-cluster $CLUSTER_NAME --certificate-authority=$CA_CERT --embed-certs=true --server=$MASTER_IP
 
 # setup user credentials
-kubectl config set-credentials $USER --client-certificate=$CLI_CERT --client-key=$CLI_KEY --embed-certs=true
+kubectl config set-credentials $USER --certificate-authority=$CA_CERT --client-certificate=$CLI_CERT --client-key=$CLI_KEY --embed-certs=true
 
 # setup the context for the user and cluster
 kubectl config set-context $CLUSTER_NAME --cluster=$CLUSTER_NAME --user=$USER
